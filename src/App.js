a835a3efd33afaf6f51f650d77c2df1802d54dd9
Chris Aquino <chris@radishmouse.com>
2021-02-23 16:40:42 -0500

    adds state to App, passes that state to TodoList

--- src/App.js
+++ src/App.js
@@ -1,17 +1,39 @@
+import React from 'react';
 import './App.css';
 
 import TodoForm from './TodoForm';
+import TodoList from './TodoList';
 
-function App() {
-  return (
-    <div className="App">
-      <TodoForm 
-        addTodo={(newTodo) => {
-          console.table(newTodo)
-        }}
-      />
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      todos: [] 
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <TodoForm 
+          addTodo={this._addTodo}
+        />
+        <TodoList todos={this.state.todos} />
+      </div>
+    );
+  }
+
+  _addTodo = (todo) => {
+    this.setState({
+      todos: [                // set it to a new array
+        ...this.state.todos,  // containing all previous todos
+        todo                  // plus the brand new todo
+      ]
+    }, () => {
+      console.log(`added new todo: ${todo.todo}`);
+    });
+  }
 }
+
 
 export default App;
--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -1,1 +1,16 @@
-// 
+import React from 'react';
+
+class TodoList extends React.Component {
+
+    render () {
+        return (
+            <ul>
+                {this.props.todos.map((t, index) => (
+                    <li key={index}>{t.todo} : {t.why}</li>
+                ))}
+            </ul>
+        );
+    }
+}
+
+export default TodoList;
